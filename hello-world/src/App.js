e9ff1afedd4276e35591b88cc45ff09f3a6cb407
Clifford18 <cliffordjohn.cj@gmail.com>
2021-08-31 10:29:30 +0300

    update App.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import Welcome from './components/Welcome';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
       <Greet/>
+      <Welcome/>
     </div>
   );
 }
