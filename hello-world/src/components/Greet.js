97c49633a7a9bc6f621a627480c2be28a2830466
Clifford18 <cliffordjohn.cj@gmail.com>
2021-09-01 12:00:14 +0300

    update Greet.js with props

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -4,6 +4,16 @@ import React from 'react'
 //   return <h1>Hello CJ</h1>
 // }
 
-export const Greet = () => <h1>Hello CJ</h1>
+export const Greet = (props) =>{ 
+  console.log(props)
+  return(
+    <div>
+      <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+      {props.children}
+    </div>
+    
+  )
+  
+}
 
 //export default Greet;
