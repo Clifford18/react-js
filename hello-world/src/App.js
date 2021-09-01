4a51cd7433ccfff9822b02a72e37dc6aa362c14a
Clifford18 <cliffordjohn.cj@gmail.com>
2021-09-01 12:03:15 +0300

    update app.js with functional(Greet) and class(Welcome) props

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,8 +8,25 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Greet/>
-      <Welcome/>
+
+      <Greet name="John" heroName="Batman">
+        <h3>This is children props</h3>
+      </Greet>
+      <Greet name="Cliff" heroName="Robin">
+        <button>Child prop Action</button>
+      </Greet>
+      <Greet name="CJ" heroName="Superman"/>
+
+      
+    <Welcome name="John" heroName="Batman">
+    <h3>This is a class components children props</h3>
+    </Welcome>
+    <Welcome name="Cliff" heroName="Robin">
+
+    </Welcome>
+    <Welcome name="CJ" heroName="Superman"/>
+
+
       <Hello/>
     </div>
   );
