250bcc3e7f62478e37dc1808a14fb52e0cc34737
Clifford18 <cliffordjohn.cj@gmail.com>
2021-09-04 12:51:30 +0300

    update  NameList.js for index as key

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import Person from './Person'
+//import Person from './Person'
 
 function NameList() {
-    //const names = ['John','CJ','Cliff']
-    const persons = [
-      {
-        id: 1,
-        name: 'Bruce',
-        age: 30,
-        skill: 'React'
-      },
-      {
-        id: 2,
-        name: 'Clark',
-        age: 25,
-        skill: 'Angular'
-      },
-      {
-        id: 3,
-        name: 'Diana',
-        age: 28,
-        skill: 'Vue'
-      }
-    ]
-    // const nameList = names.map(name => <h2>{name}</h2>)
-    const personList = persons.map(person => <Person key={person.id} person={person}/>)
+    const names = ['John','CJ','Cliff']
+    // const persons = [
+    //   {
+    //     id: 1,
+    //     name: 'Bruce',
+    //     age: 30,
+    //     skill: 'React'
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Clark',
+    //     age: 25,
+    //     skill: 'Angular'
+    //   },
+    //   {
+    //     id: 3,
+    //     name: 'Diana',
+    //     age: 28,
+    //     skill: 'Vue'
+    //   }
+    // ]
+    const nameList = names.map((name,index) => <h2 key={index}>{name}</h2>)
+    // const personList = persons.map(person => <Person key={person.id} person={person}/>)
   return (
     <div>
       {/* <h2>{names[0]}</h2>
@@ -34,8 +34,8 @@ function NameList() {
         names.map(name => <h2>{name}</h2>)
       }
       */}
-      {/* {nameList} */}
-      {personList}
+      {nameList}
+      {/* {personList} */}
     </div>
   )
 }
